52facac644497985dd3f95519209bd934c085915
CGandGameEngineLearner <1353321741@qq.com>
2023-11-10 10:34:04 +0800

    feat:first commit

---
+++ Plugins/RankingListPlugin/Source/RankingListPlugin/Public/UserWidgets/RankingListUserWidget.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RankingListUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RANKINGLISTPLUGIN_API URankingListUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
